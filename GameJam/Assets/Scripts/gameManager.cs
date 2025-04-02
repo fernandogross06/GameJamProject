1458a645bb39fff18c4f97874aa4a57b9075d089
fernandogross06 <80482002+fernandogross06@users.noreply.github.com>
2025-04-01 20:47:40 -0600

    Cambios pequeños
    Cambios a funcionamiento de raycast y de caja de dialogo

--- GameJam/Assets/Scripts/gameManager.cs
+++ GameJam/Assets/Scripts/gameManager.cs
@@ -11,7 +11,7 @@ public class gameManager : MonoBehaviour
     public TextMeshProUGUI textPrefab;
     public Transform canvasTransform;
     TextMeshProUGUI interactText;
-    bool interacting;
+    bool interacting = false;
 
     void Start()
     {
@@ -30,47 +30,51 @@ public class gameManager : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, raycastDistance)) //Actively creates a ray using the above set perameters at the predeterminded distance
         {
-            interactText.text = "hola";
+           
             interactText.transform.SetParent(canvasTransform, false);
             //Item Raycast Detection
-            if (hit.collider.CompareTag("interact"))//Checking if the Raycast has hit a collider with the tag of note
+            if (interacting == false)
             {
-                interactText.text = "Presiona [I] para interactuar."; //Setting the Interaction Text to let the player know they are now hovering an interactable object
-                if (interacting)
+                
+                if (hit.collider.CompareTag("interact"))//Checking if the Raycast has hit a collider with the tag of note
                 {
-                    interactText.text = "";
+                    print(interacting);
+
+                    interactText.text = "Presiona [I] para interactuar."; //Setting the Interaction Text to let the player know they are now hovering an interactable object
+                    print(interactText.text);
+                    if (Input.GetKeyDown(KeyCode.I))//Check if the player has pressed the Interaction button
+                    {
+                        interacting = true;
+                        Debug.Log("[I] Was Pressed while looking at a note, lets open the note.");
+                        interactText.text = "";
+
+                        //Add your Note Method/UI here - for example (if the note has a script attached to it)
+                        movement.enabled = false;
+                        inkTest.enabled = true;
+                        inkTest.Reset();
+                        cameraMovement.enabled = false;
+
+                        Cursor.lockState = CursorLockMode.None;
+                        Cursor.visible = true;
+
+                    }
                 }
-                    
-                if (Input.GetKeyDown(KeyCode.I))//Check if the player has pressed the Interaction button
+
+                else //If nothing at all with an above tag was hit with the Raycast within the specified distance then run this
                 {
-                    interacting = true;
-                    Debug.Log("[I] Was Pressed while looking at a note, lets open the note.");
-                    interactText.text = "";
-
-                    //Add your Note Method/UI here - for example (if the note has a script attached to it)
-                    movement.enabled = false;
-                    inkTest.enabled = true;
-                    inkTest.Reset();
-                    cameraMovement.enabled = false;
-
-                    Cursor.lockState = CursorLockMode.None;
-                    Cursor.visible = true;
-
+                    if (interactText.text != "")//If the interactText is not already set as nothing then set it to nothing - this is to help optimise and save from constantly spamming this request
+                    {
+                        interactText.text = ""; //Removing the text as nothing was detected by the raycast
+                    }
+                    interacting = false;
                 }
             }
-            /*else if (hit.collider.CompareTag(""))
-            {
-                //Add a different object here if you choose to want more objects to be able to be detected by the Raycast such as doors etc
-            }*/
 
-            else //If nothing at all with an above tag was hit with the Raycast within the specified distance then run this
-            {
-                if (interactText.text != "")//If the interactText is not already set as nothing then set it to nothing - this is to help optimise and save from constantly spamming this request
-                {
-                    interactText.text = ""; //Removing the text as nothing was detected by the raycast
-                }
-                interacting = false;
-            }
+            print(interacting);
+            //interactText.text = "";
+
+
+
         }
 
 
@@ -80,7 +84,7 @@ public class gameManager : MonoBehaviour
             inkTest.enabled = false;
             cameraMovement.enabled = true;
 
-            
+            interacting = false;
 
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
